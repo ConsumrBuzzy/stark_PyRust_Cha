9cde98cc373819f67d7bf2cd2dae09ca53f4a546
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-02-11 23:43:00 -0500

    feat: Add a new module to fetch ETH to STRK swap quotes from AVNU for gas refueling.

--- python-logic/gas_refuel.py
+++ python-logic/gas_refuel.py
@@ -14,12 +14,12 @@ def get_swap_quote(amount_eth=0.002):
     ETH_ADDRESS = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
     STRK_ADDRESS = "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
     
-    amount_hex = hex(int(amount_eth * 10**18))
+    amount_decimal = str(int(amount_eth * 10**18))
     
     params = {
         "sellTokenAddress": ETH_ADDRESS,
         "buyTokenAddress": STRK_ADDRESS,
-        "sellAmount": amount_hex,
+        "sellAmount": amount_decimal,
         "size": 1 # Just get the best quote
     }
     
