ee14663de4a7ca689524daed8cf2b45ef1bb4f07
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-02-13 17:12:37 -0500

    test: Add web3 module stubs to test_ops_network_checks.py, implementing DummyHTTPProvider/DummyMiddlewareOnion/DummyEth/DummyWeb3 classes to satisfy src.foundation.network imports without requiring web3 package installation

--- tests/test_ops_network_checks.py
+++ tests/test_ops_network_checks.py
@@ -1,7 +1,45 @@
 import asyncio
+import sys
+import types
 from decimal import Decimal
 
 import pytest
+
+
+# Stub minimal web3 modules to satisfy src.foundation.network imports
+if "web3" not in sys.modules:
+    web3_stub = types.ModuleType("web3")
+    middleware_stub = types.ModuleType("web3.middleware")
+
+    class DummyHTTPProvider:
+        def __init__(self, _url):
+            pass
+
+    class DummyMiddlewareOnion:
+        def inject(self, *_args, **_kwargs):
+            return None
+
+    class DummyEth:
+        def __init__(self):
+            self.block_number = 0
+
+        def get_balance(self, _addr):
+            return 0
+
+    class DummyWeb3:
+        def __init__(self, *_args, **_kwargs):
+            self.middleware_onion = DummyMiddlewareOnion()
+            self.eth = DummyEth()
+
+        def from_wei(self, value, _unit):
+            return value
+
+    web3_stub.Web3 = DummyWeb3
+    web3_stub.HTTPProvider = DummyHTTPProvider
+    middleware_stub.ExtraDataToPOAMiddleware = object
+
+    sys.modules["web3"] = web3_stub
+    sys.modules["web3.middleware"] = middleware_stub
 
 from src.ops import network_checks
 from src.ops.env import OpsConfig
